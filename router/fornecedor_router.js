226bd041181bf4526612d25d91752a64aca6aed1
Francisco Rech <frrech@outlook.com>
2025-11-04 19:13:45 -0300

    Fixing routing

--- router/fornecedor_router.js
+++ router/fornecedor_router.js
@@ -2,10 +2,10 @@ const fornecedor_controller = require('../controller/fornecedor_controller');
 const express = require('express');
 const router = express.Router();
 
-router.post('/fornecedores', fornecedor_controller.createFornecedor);
-router.get('/fornecedores', fornecedor_controller.getAllFornecedores);
-router.get('/fornecedores/:id', fornecedor_controller.getFornecedorById);
-router.put('/fornecedores/:id', fornecedor_controller.updateFornecedor);
-router.delete('/fornecedores/:id', fornecedor_controller.deleteFornecedor);
+router.post('/', fornecedor_controller.createFornecedor);
+router.get('/', fornecedor_controller.getAl);
+router.get('/:id', fornecedor_controller.getFornecedorById);
+router.put('/:id', fornecedor_controller.updateFornecedor);
+router.delete('/:id', fornecedor_controller.deleteFornecedor);
 
 module.exports = router;
